84518c3533bc7ac6a83a80e170b22b12acd55fe1
Tamashiro syohei <e175752@ie.u-ryukyu.ac.jp>
2017-11-11 23:40:07 +0900

    add test code for Enemy.attack() attackを改良した。

--- src/main/jp/ac/uryukyu/ie/e175752/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e175752/Enemy.java
@@ -46,9 +46,12 @@ public class Enemy {
      * @param hero 攻撃対象
      */
     public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+        if( dead == false && hitPoint >0) {
+            int damage = (int)(Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
+
+        }
     }
 
     /**
